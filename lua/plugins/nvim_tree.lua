f0b6daaf86a82ab2076682f9b8d263c074930e13
ClericalAid <christian.lee93@gmail.com>
2025-03-16 13:13:29 -0400

    Add hotkey to toggle nvim-tree

--- lua/plugins/nvim_tree.lua
+++ lua/plugins/nvim_tree.lua
@@ -6,12 +6,36 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
+    require("nvim-tree").setup {
+      update_focused_file = {
+        enable = true,
+        update_cwd = false,
+        ignore_list = {},
+      },
+      view = {
+        relativenumber = true,
+        float = {
+          enable = true,
+          open_win_config = {
+            relative = "editor",
+            border = "rounded",
+            width = 80,
+            height = 40,
+            row = 1,
+            col = 1,
+          },
+        },
+      },
+      help = {
+        sort_by = "key",
+      },
+    } -- require("nvim-tree").setup
     -- disable netrw at the very start of your init.lua
     vim.g.loaded_netrw = 1
     vim.g.loaded_netrwPlugin = 1
 
     -- optionally enable 24-bit colour
     vim.opt.termguicolors = true
-    require("nvim-tree").setup {}
+    vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
   end,
 }
